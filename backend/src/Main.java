6d5dcfc9faf8285e8d60bc7b137d0a645accf8b2
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-01-05 12:02:08 -0300

    feat: Adicionada a entidade Usuario

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
+import entidades.Contato;
+import entidades.Endereco;
+import entidades.Usuario;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat formato3 = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataFormatada3 = formato3.parse("21/07/2021");
+        //System.out.println(formato3.format(dataFormatada3));
+
+        ArrayList<Contato> listaContatos = new ArrayList<>();
+        ArrayList<Endereco> listaEndereco = new ArrayList<>();
+
+        Usuario usuario = new Usuario("Maria Joana Texeira", "06497895622", formato3.parse("21/07/2090"),  listaEndereco, listaContatos, "mariatexeira@gmail.com", null);
+
+        System.out.println(usuario);
     }
 }
--- backend/src/entidades/Usuario.java
+++ backend/src/entidades/Usuario.java
@@ -1,4 +1,99 @@
 package entidades;
 
+import enums.TipoUsuarioEnum;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Usuario {
+
+    private String nome;
+    private String cpf;
+    private Date dataDeNascimento;
+    private ArrayList<Endereco> enderecos;
+    private ArrayList<Contato> contatos;
+    private String email;
+    private TipoUsuarioEnum tipo;
+
+    public Usuario () {}
+
+    public Usuario (String nome, String cpf, Date dataDeNascimento, ArrayList<Endereco> enderecos, ArrayList<Contato> contatos, String email, TipoUsuarioEnum tipo) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataDeNascimento = dataDeNascimento;
+        this.enderecos = new ArrayList<>();
+        this.contatos = new ArrayList<>();
+        this.email = email;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public ArrayList<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(ArrayList<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public ArrayList<Contato> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(ArrayList<Contato> contatos) {
+        this.contatos = contatos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipoUsuarioEnum getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoUsuarioEnum tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", dataDeNascimento=" + dataDeNascimento +
+                ", enderecos=" + enderecos +
+                ", contatos=" + contatos +
+                ", email='" + email + '\'' +
+                ", tipo=" + tipo +
+                '}';
+    }
+
 }
