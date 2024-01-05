1d89e81648804559732cc47c9d1e7de2655df6d0
Ga-Zampieri <gabriel_zampieri@hotmail.com>
2024-01-05 18:17:25 -0300

    feat: adcicionado entidade contato e seus respectivos metodos acessores.

--- backend/src/entidades/Contato.java
+++ backend/src/entidades/Contato.java
@@ -1,4 +1,32 @@
 package entidades;
 
+import enums.TipoEnum;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Contato {
+    private static final AtomicInteger count = new AtomicInteger(0);
+    private long id;
+    private String descricao;
+    private String telefone;
+    private TipoEnum tipo;
+
+    public Contato() {
+        this.id = count.incrementAndGet();
+    }
+
+    public Contato(String descricao, String telefone, TipoEnum tipo) {
+        this.id = count.incrementAndGet();
+        this.descricao = descricao;
+        this.telefone = telefone;
+        this.tipo = tipo;
+    }
+
+    public long getId() { return id; }
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+    public TipoEnum getTipo() { return tipo; }
+    public void setTipo(TipoEnum tipo) { this.tipo = tipo; }
 }
