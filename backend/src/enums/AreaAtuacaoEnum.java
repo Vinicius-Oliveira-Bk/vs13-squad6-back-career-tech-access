417e274e81f927847b0b511da1d5f050125cd5c0
lucarauj <lukas.martins193@gmail.com>
2024-01-06 19:20:26 -0300

    feat: Adicionando classe ProfissionalMentor, interface IDocumentacaoProfissional, enum AreaAtuacaoEnum e enum NivelExperienciaEnum

---
+++ backend/src/enums/AreaAtuacaoEnum.java
@@ -0,0 +1,17 @@
+package enums;
+
+public enum AreaAtuacaoEnum {
+        TI,
+        SAUDE,
+        EDUCACAO,
+        FINANCAS,
+        MARKETING,
+        JURIDICO,
+        ENGENHARIA,
+        DESIGN,
+        COMERCIO,
+        MEIO_AMBIENTE,
+        CONSULTORIA,
+        RH,
+        OUTROS;
+}
