f4f11c7e7b6f7ced9da89c06b1b7ba6ca426f60b
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-01-06 13:29:25 -0300

    feat: Adicionado os métodos vincularContato e vincularEndereco,para vincular os contadts dcscom comcccaasdaaaasdfffgos ao usuário

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -3,44 +3,32 @@ import servicos.ContatoServico;
 import entidades.Usuario;
 import servicos.UsuarioServico;
 
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 public class Main {
     public static void main(String[] args) {
-        
-        Contato c = new Contato();
-        Contato c1 = new Contato();
-        Contato c2 = new Contato();
-        Contato c3 = new Contato();
-        Contato c4 = new Contato();
 
-        ContatoServico cs = new ContatoServico();
-        cs.cadastrar(c);
-        cs.cadastrar(c1);
-        cs.cadastrar(c2);
-        cs.cadastrar(c3);
-        cs.cadastrar(c4);
+        UsuarioServico usuarioServico = new UsuarioServico();
+        ContatoServico contatoServico = new ContatoServico();
 
-        System.out.println(cs.listarTodos());
-        try {
-            System.out.println(cs.listarUm(4));
-            System.out.println(cs.listarUm(5));
-        } catch (NoSuchElementException e) {
-            System.out.println(e.getMessage());
-        }
-      
-        Usuario usuario1 = new Usuario(1, "Maria Joana Texeira", "06497895622", "05-12-1990",  null, null, "mariatexeira@gmail.com", null);
+        // Cadastrando alguns usuários
+        Usuario usuario1 = new Usuario(1, "Maria Joana Texeira", "06497895622", "05-12-1990", null, null,  "mariatexeira@gmail.com", null);
         Usuario usuario2 = new Usuario(2, "Carlos Pinto da Silva", "23569548966", "31-02-2000",  null, null, "carlossilva@gmail.com", null);
 
-        UsuarioServico contatoServico = new UsuarioServico();
-        contatoServico.cadastrar(usuario1);
-        contatoServico.cadastrar(usuario2);
+        usuarioServico.cadastrar(usuario1);
 
-        contatoServico.listarTodos();
-//        contatoServico.listarUm(3);
-//        contatoServico.atualizar(1, usuario1);
-//
-//        contatoServico.listarTodos();
-//        contatoServico.deletar(1);
-//        contatoServico.listarTodos();
+        // Adicionando alguns contatos no usuário
+        Contato contato1 = new Contato("teste1", "99999999999", null);
+        Contato contato2 = new Contato("teste2", "22222222222", null);
+
+        // Vinculando o contato com o usuário
+        usuarioServico.vincularContato(usuario1, contato1);
+        usuarioServico.vincularContato(usuario1, contato2);
+
+        usuarioServico.listarTodos();
+
+
 
     }
   
