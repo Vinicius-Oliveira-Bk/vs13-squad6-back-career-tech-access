a2976beb1ed70c891bef73b8140aa0c807974ca8
Luiz Fellipe de Almeida R. Barbosa <luizfellipedearb@gmail.com>
2024-01-06 18:43:59 -0300

    feat: adicionando classe Pcd, seus construtores e seus respectivos getters e setters

--- backend/src/entidades/Pcd.java
+++ backend/src/entidades/Pcd.java
@@ -1,4 +1,40 @@
 package entidades;
 
-public class Pcd {
+import enums.TipoUsuarioEnum;
+
+import java.util.ArrayList;
+
+public class Pcd extends Cliente {
+    private String tipoDeficiencia;
+    private String certificadoDeficienciaGov;
+
+    public Pcd() {
+    }
+
+    public Pcd(String tipoDeficiencia, String certificadoDeficienciaGov) {
+        this.tipoDeficiencia = tipoDeficiencia;
+        this.certificadoDeficienciaGov = certificadoDeficienciaGov;
+    }
+
+    public Pcd(long id, String nome, String cpf, String dataDeNascimento, ArrayList<Endereco> enderecos, ArrayList<Contato> contatos, String email, TipoUsuarioEnum tipo, String plano, String interesses, String imagemDocummento, boolean controleParental, boolean acessoPcd, String tipoDeficiencia, String certificadoDeficienciaGov) {
+        super(id, nome, cpf, dataDeNascimento, enderecos, contatos, email, tipo, plano, interesses, imagemDocummento, controleParental, acessoPcd);
+        this.tipoDeficiencia = tipoDeficiencia;
+        this.certificadoDeficienciaGov = certificadoDeficienciaGov;
+    }
+
+    public String getTipoDeficiencia() {
+        return tipoDeficiencia;
+    }
+
+    public void setTipoDeficiencia(String tipoDeficiencia) {
+        this.tipoDeficiencia = tipoDeficiencia;
+    }
+
+    public String getCertificadoDeficienciaGov() {
+        return certificadoDeficienciaGov;
+    }
+
+    public void setCertificadoDeficienciaGov(String certificadoDeficienciaGov) {
+        this.certificadoDeficienciaGov = certificadoDeficienciaGov;
+    }
 }
