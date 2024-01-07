224bd1b06a2b3bcb382ffe8b5e43ab378e719b0b
Ga-Zampieri <gabriel_zampieri@hotmail.com>
2024-01-06 23:10:13 -0300

    refactor: removendo atributo inutilizado do construtor

--- backend/src/entidades/ProfissionalMentor.java
+++ backend/src/entidades/ProfissionalMentor.java
@@ -16,7 +16,7 @@ public class ProfissionalMentor extends Usuario implements IDocumentacaoProfissi
 
     public ProfissionalMentor() {}
 
-    public ProfissionalMentor(long id, String nome, String cpf, String dataDeNascimento, ArrayList<Endereco> enderecos, ArrayList<Contato> contatos, String email, TipoUsuarioEnum tipo, AreaAtuacaoEnum areaAtuacao, NivelExperienciaEnum nivelExperienciaEnum, String carteiraDeTrabalho, ArrayList<String> certificadosDeCapacitacao) {
+    public ProfissionalMentor(String nome, String cpf, String dataDeNascimento, ArrayList<Endereco> enderecos, ArrayList<Contato> contatos, String email, TipoUsuarioEnum tipo, AreaAtuacaoEnum areaAtuacao, NivelExperienciaEnum nivelExperienciaEnum, String carteiraDeTrabalho, ArrayList<String> certificadosDeCapacitacao) {
         super(nome, cpf, dataDeNascimento, enderecos, contatos, email, tipo);
         this.areaAtuacao = areaAtuacao;
         this.nivelExperienciaEnum = nivelExperienciaEnum;
