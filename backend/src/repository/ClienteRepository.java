7095a691ff2f831e4de0aa47fd8b7c3271a56495
Luiz Fellipe de Almeida R. Barbosa <luizfellipedearb@gmail.com>
2024-01-11 23:12:17 -0300

    feat: adicionando classe ClienteServico

---
+++ backend/src/repository/ClienteRepository.java
@@ -0,0 +1,12 @@
+package repository;
+
+import exceptions.BancoDeDadosException;
+import model.entidades.Cliente;
+import model.enums.PlanoEnum;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClienteRepository {
+}
