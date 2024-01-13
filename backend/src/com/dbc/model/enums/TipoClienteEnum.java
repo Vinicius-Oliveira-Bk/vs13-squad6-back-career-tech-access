a9ad8b94550bc9c93c47646ca53fdab7c6070e7a
Bruno Scarpari <brunoomoraes686@gmail.com>
2024-01-13 18:00:24 -0300

    feat: Scripts sql inicias + docs

--- backend/src/com/dbc/model/enums/TipoClienteEnum.java
+++ backend/src/com/dbc/model/enums/TipoClienteEnum.java
@@ -3,7 +3,9 @@ package com.dbc.model.enums;
 public enum TipoClienteEnum {
     ESTUDANTE (1),
     PCD (2),
-    PROFISSIONAL_REALOCAO (3);
+    PROFISSIONAL_REALOCACAO (3),
+    MENTOR(4);
+
 
     private final int tipoCliente;
 
