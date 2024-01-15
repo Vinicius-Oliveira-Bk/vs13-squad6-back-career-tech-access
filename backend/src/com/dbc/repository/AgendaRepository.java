ccece8ded320c55042c9ac77577af304a2b37d93
Ga-Zampieri <gabriel_zampieri@hotmail.com>
2024-01-14 21:36:07 -0300

    fix: correção de tabelas para funcionamento do agendamento e correção nas operações de AgendaRepository

--- backend/src/com/dbc/repository/AgendaRepository.java
+++ backend/src/com/dbc/repository/AgendaRepository.java
@@ -16,7 +16,7 @@ public class AgendaRepository implements IRepository<Long, Agenda>{
 
     @Override
     public Long getProximoId(Connection connection) throws SQLException {
-        String sql = "SELECT SEQ_AGENDA.NEXTVAL SEQUENCE_AGENDA FROM DUAL";
+        String sql = "SELECT SEQ_AGENDA.NEXTVAL AS SEQUENCE_AGENDA FROM DUAL";
         Statement stmt = connection.createStatement();
         ResultSet result = stmt.executeQuery(sql);
         if (result.next()) {
@@ -34,7 +34,7 @@ public class AgendaRepository implements IRepository<Long, Agenda>{
             agenda.setId(novoId);
 
             String sql = "INSERT INTO AGENDA\n" +
-                    "(ID, ID_CLIENTE, ID_MENTOR, DATA_HORA_INICIO, DATA_HORA_FIM, STATUS)\n" +
+                    "(ID, ID_CLIENTE, ID_MENTOR, DATA_INICIO, DATA_FIM, STATUS)\n" +
                     "VALUES(?, ?, ?, ?, ?, ?)\n";
 
             PreparedStatement stmt = con.prepareStatement(sql);
@@ -75,15 +75,15 @@ public class AgendaRepository implements IRepository<Long, Agenda>{
 
             while (!result.next()) {
                 Agenda agenda = new Agenda();
-                agenda.setId(result.getLong("ID_PESSOA"));
+                agenda.setId(result.getLong("ID"));
                 agenda.setCliente(cs.listarUm(result.getLong("ID_CLIENTE")));
-                agenda.setProfissionalMentor(ps.listarUm(result.getLong("ID_PROFISSIONAL")));
-                agenda.setDataHoraInicio(result.getTimestamp("DATA_HORA_INICIO").toLocalDateTime());
-                agenda.setDataHoraFim(result.getTimestamp("DATA_HORA_FIM").toLocalDateTime());
-                agenda.setStatusAgendaEnum(StatusAgendaEnum.fromValor(result.getInt("STATUS_AGENDA")));
+                agenda.setProfissionalMentor(ps.listarUm(result.getLong("ID_MENTOR")));
+                agenda.setDataHoraInicio(result.getTimestamp("DATA_INICIO").toLocalDateTime());
+                agenda.setDataHoraFim(result.getTimestamp("DATA_FIM").toLocalDateTime());
+                agenda.setStatusAgendaEnum(StatusAgendaEnum.fromValor(result.getInt("STATUS")));
                 agendamentos.add(agenda);
             }
-        } catch (SQLException e) {
+        } catch (SQLException | NullPointerException e) {
             throw new BancoDeDadosException(e.getCause());
         } finally {
             try {
@@ -107,12 +107,12 @@ public class AgendaRepository implements IRepository<Long, Agenda>{
             ResultSet result = stmt.executeQuery(sql);
             if (result.next()) {
                 Agenda agendamento = new Agenda();
-                agendamento.setId(result.getLong("ID_PESSOA"));
+                agendamento.setId(result.getLong("ID"));
                 agendamento.setCliente(cs.listarUm(result.getLong("ID_CLIENTE")));
-                agendamento.setProfissionalMentor(ps.listarUm(result.getLong("ID_PROFISSIONAL")));
-                agendamento.setDataHoraInicio(result.getTimestamp("DATA_HORA_INICIO").toLocalDateTime());
-                agendamento.setDataHoraFim(result.getTimestamp("DATA_HORA_FIM").toLocalDateTime());
-                agendamento.setStatusAgendaEnum(StatusAgendaEnum.fromValor(result.getInt("STATUS_AGENDA")));
+                agendamento.setProfissionalMentor(ps.listarUm(result.getLong("ID_MENTOR")));
+                agendamento.setDataHoraInicio(result.getTimestamp("DATA_INICIO").toLocalDateTime());
+                agendamento.setDataHoraFim(result.getTimestamp("DATA_FIM").toLocalDateTime());
+                agendamento.setStatusAgendaEnum(StatusAgendaEnum.fromValor(result.getInt("STATUS")));
                 return agendamento;
             }
             return null;
@@ -138,9 +138,9 @@ public class AgendaRepository implements IRepository<Long, Agenda>{
             StringBuilder sql = new StringBuilder();
             sql.append("UPDATE AGENDA SET ");
             sql.append(" id_cliente = ?,");
-            sql.append(" data_hora_inicio = ?,");
-            sql.append(" data_hora_fim = ? ");
-            sql.append(" status_agendamento = ? ");
+            sql.append(" data_inicio = ?,");
+            sql.append(" data_fim = ? ");
+            sql.append(" status = ? ");
             sql.append(" WHERE id = ? ");
 
             PreparedStatement stmt = con.prepareStatement(sql.toString());
