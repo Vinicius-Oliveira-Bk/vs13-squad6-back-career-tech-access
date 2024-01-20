09af8447cacf63cd7eef14735b4ed1434ed8db5f
lucarauj <lukas.martins193@gmail.com>
2024-01-20 17:11:12 -0300

    feat: Adiciona CRUD Contatos utilizando springweb

--- backend/src/main/java/br/com/dbc/vemser/services/ContatoService.java
+++ backend/src/main/java/br/com/dbc/vemser/services/ContatoService.java
@@ -1,56 +1,71 @@
 package br.com.dbc.vemser.services;
 
+
 import br.com.dbc.vemser.exceptions.BancoDeDadosException;
+import br.com.dbc.vemser.exceptions.RegraDeNegocioException;
+import br.com.dbc.vemser.model.dtos.request.ContatoRequestDTO;
+import br.com.dbc.vemser.model.dtos.response.ContatoResponseDTO;
 import br.com.dbc.vemser.model.entities.Contato;
 import br.com.dbc.vemser.repository.ContatoRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
 public class ContatoService {
 
-    private ContatoRepository contatoRepository;
+    private final ContatoRepository contatoRepository;
+    private final ObjectMapper objectMapper;
 
-    public ContatoService() {
-        contatoRepository = new ContatoRepository();
+
+    public ContatoResponseDTO create(ContatoRequestDTO contatoRequestDTO) throws Exception {
+        Contato contato = objectMapper.convertValue(contatoRequestDTO, Contato.class);
+        contatoRepository.create(contato);
+        ContatoResponseDTO contatoResponseDTO = objectMapper.convertValue(contato, ContatoResponseDTO.class);
+        return contatoResponseDTO;
     }
 
-    public void cadastrar(Contato contato) {
+    public List<ContatoResponseDTO> listAll() throws BancoDeDadosException {
+        List<Contato> contatosEntity= contatoRepository.getAll();
+        List<ContatoResponseDTO> contatosResponseDTO = contatosEntity.stream()
+                .map(contatoEntity -> objectMapper.convertValue(contatoEntity, ContatoResponseDTO.class))
+                .collect(Collectors.toList());
+        return contatosResponseDTO;
+    }
+
+    public ContatoResponseDTO update(Long id, ContatoRequestDTO contatoRequestDTO) throws Exception {
+        Contato buscaContato = getContato(id);
+
+        Contato contatoEntity = objectMapper.convertValue(contatoRequestDTO, Contato.class);
+        contatoRepository.update(id, contatoEntity);
+        contatoEntity.setId(id);
+
+        ContatoResponseDTO contatoResponseDTO = objectMapper.convertValue(contatoEntity, ContatoResponseDTO.class);
+        return contatoResponseDTO;
+    }
+
+    public void delete(Long id) throws Exception {
+        Contato buscaContato = getContato(id);
+        contatoRepository.delete(id);
+    }
+
+    public ContatoResponseDTO listById(Long id) throws Exception {
+        Contato contatoEntity = getContato(id);
+        ContatoResponseDTO contatoResponseDTO = objectMapper.convertValue(contatoEntity, ContatoResponseDTO.class);
+        return contatoResponseDTO;
+    }
+
+    private Contato getContato(Long id) throws RegraDeNegocioException {
         try {
-            contatoRepository.cadastrar(contato);
-            System.out.println("✅ Contato Adicionado com sucesso! " + contato);
-        } catch (BancoDeDadosException e) {
-            e.printStackTrace();
+            Contato contatoRecuperado = contatoRepository.getById(id);
+            return contatoRecuperado;
+        } catch (Exception ex) {
+            throw new RegraDeNegocioException("Nenhum contato encontrado para o Id: " + id);
         }
     }
 
-    public void listarTodos() {
-        try {
-            contatoRepository.listar().forEach(System.out::println);
-        } catch (BancoDeDadosException e) {
-            e.printStackTrace();
-        }
-    }
-    public void listarUm(Long id) {
-        try {
-            contatoRepository.listarUm(id);
-        } catch (BancoDeDadosException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void atualizar(Long id, Contato contato) {
-        try {
-            contatoRepository.atualizar(id, contato);
-            System.out.println("✅ Contato Editado com Sucesso");
-        } catch (BancoDeDadosException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void remover(Long id) {
-        try {
-            contatoRepository.remover(id);
-            System.out.println("✅ Contato Removido com Sucesso");
-        } catch (BancoDeDadosException e) {
-            e.printStackTrace();
-        }
-    }
 }
