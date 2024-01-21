e2fb06dec72f19eba94dcb5855e6e97d4bf82fff
lucarauj <lukas.martins193@gmail.com>
2024-01-21 20:14:01 -0300

    feat: Adiciona CRUD Cliente utilizando springweb

---
+++ backend/src/main/java/br/com/dbc/vemser/model/dtos/response/ClienteResponseDTO.java
@@ -0,0 +1,16 @@
+package br.com.dbc.vemser.model.dtos.response;
+
+import br.com.dbc.vemser.model.enums.PlanoEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteResponseDTO {
+    private Long id;
+    private Long idUsuario;
+    private PlanoEnum tipoPlano;
+    private Character controleParental;
+}
