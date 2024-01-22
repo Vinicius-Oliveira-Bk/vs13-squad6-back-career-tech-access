00eb7f3d1e4ef57668e83ba75e79bbe78b7c0abf
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-01-22 09:27:55 -0300

    refactor: Remoção das anotações da classe usuário

--- backend/src/main/java/br/com/dbc/vemser/model/entities/Usuario.java
+++ backend/src/main/java/br/com/dbc/vemser/model/entities/Usuario.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -16,29 +15,12 @@ import java.util.ArrayList;
 public class Usuario implements IVinculosUsuario {
 
     private Long id;
-
-    @NotBlank(message = "O campo não pode ser nulo, vazio ou conter apenas espaços em branco")
     private String nome;
-
-    @NotNull(message = "A data de nascimento não pode ser nula")
-    @Past(message = "A data de nascimento deve ser uma data passada")
     private LocalDate dataNascimento;
-
-    @NotBlank(message = "O campo não pode ser nulo, vazio ou conter apenas espaços em branco")
-    @Size(min = 11, max = 11, message = "O CPF deve conter exatamente 11 dígitos numéricos")
     private String cpf;
-
-    @NotNull(message = "O email não pode ser nulo")
-    @Email(message = "Email inválido. Formato: email@provedor")
     private String email;
-
-    @NotNull(message = "Acesso PCD inválido! O campo deve ser: 'S' ou 'N'")
     private Character acessoPcd;
-
-
-    @NotNull(message = "Tipo de usuário inválido. Escolha: MENTOR, ESTUDANTE, PCD OU PROFISSIONAL_RELOCACAO ")
     private TipoUsuarioEnum tipoUsuario;
-
     private String interesses;
     private ArrayList<Contato> contatos;
     private ArrayList<Endereco> enderecos;
