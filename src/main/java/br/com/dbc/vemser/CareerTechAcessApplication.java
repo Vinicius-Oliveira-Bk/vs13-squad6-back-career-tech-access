71541046284b9a728e2928ca2b47212d9911567b
Ga-Zampieri <gabriel_zampieri@hotmail.com>
2024-02-19 16:57:40 -0300

    feat: add mongo-db to application

--- src/main/java/br/com/dbc/vemser/CareerTechAcessApplication.java
+++ src/main/java/br/com/dbc/vemser/CareerTechAcessApplication.java
@@ -2,8 +2,12 @@ package br.com.dbc.vemser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
+@EnableJpaRepositories
 public class CareerTechAcessApplication {
 
 	public static void main(String[] args) {
